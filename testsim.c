9565d291e5cad705d8d8069114950ebddd05ae46
Dathan Huang <dh@schrius.com>
2017-03-09 14:54:54 -0500

    Add files via upload

---
+++ testsim.c
@@ -0,0 +1,27 @@
+/*
+ * testsim.c
+ *
+ *  Created on: Mar 7, 2017
+ *      Author: dathan
+ */
+#include <stdio.h> /* fprintf */
+#include <stdlib.h> /* atoi */
+#include <unistd.h> /* sleep */
+
+int main(int argc, char *argv[]) {
+	int n, m;
+
+	if ( argc != 3 ) {
+		fprintf(stderr, "Usage %s sleep_time num_loops\n", argv[0]);
+		return 1;
+	}
+
+	n = atoi(argv[1]);
+
+	for ( m = atoi(argv[2]); m > 0; m--) {
+		sleep(n);
+		fprintf(stderr, "pid: %d\n", getpid());
+	}
+
+	return 0;
+}
