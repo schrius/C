8fcafa8cc447fdc131d2b1c63cba47b2c022854a
Dathan Huang <dh@schrius.com>
2017-03-09 14:56:56 -0500

    Update testsim.c

--- testsim.c
+++ testsim.c
@@ -9,17 +9,17 @@
 #include <unistd.h> /* sleep */
 
 int main(int argc, char *argv[]) {
-	int n, m;
+	int stime, repeat;
 
 	if ( argc != 3 ) {
-		fprintf(stderr, "Usage %s sleep_time num_loops\n", argv[0]);
+		fprintf(stderr, "Usage %s [sleep time] [loops]\n", argv[0]);
 		return 1;
 	}
 
-	n = atoi(argv[1]);
+	stime = atoi(argv[1]);
 
-	for ( m = atoi(argv[2]); m > 0; m--) {
-		sleep(n);
+	for (int i = 0; i < repeat; i++) {
+		sleep(stime);
 		fprintf(stderr, "pid: %d\n", getpid());
 	}
 
